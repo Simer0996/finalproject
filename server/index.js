46682d3da779c6b5e89fb38ac46a550d8ac6f50c
Simer0996 <singhsimer09@gmail.com>
2022-12-08 01:30:01 -0800

    fixing server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,36 @@
+// const { ApolloServer } = require('apollo-server');
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+
+// const typeDefs = require('./Graphql/typeDefs');
+// const resolvers = require('./Graphql/resolvers/index');
+
+
+// dotenv.config();
+
+// const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     context: ({ req }) => ({ req })
+// });
+
+// mongoose.connect(`${process.env.MONGODB}`, { useNewUrlParser: true })
+//     .then(() => {
+//         console.log('MongoDB Connected')
+
+//         server.listen({ port: 5000 }).then(res => {
+//             console.log(`Server running at ${res.url}`)
+//         })
+//     })
+
+
+
+import express from 'express'
+// import { ApolloServer } from 'apollo-server-express';
+import http from 'http'
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { typeDefs, resolvers } from './src/schema.js';
+
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -11,7 +44,9 @@ dotenv.config();
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({ req }) => ({ req })
+    context: ({ req }) => ({ req }),
+    playground: true,
+    introspection: true,
 });
 
 mongoose.connect(`${process.env.MONGODB}`, { useNewUrlParser: true })
@@ -22,3 +57,4 @@ mongoose.connect(`${process.env.MONGODB}`, { useNewUrlParser: true })
             console.log(`Server running at ${res.url}`)
         })
     })
+
